945e1509a0c3b37fb1e7655752b72e0dba382e02
Joru <jojooapgv@gmail.com>
2023-01-22 20:18:40 +0400

    even1-N

---
+++ HW1/4even/Even until N.cs
@@ -0,0 +1,13 @@
+﻿int a, n;
+Console.WriteLine("Input number");
+string? num = Console.ReadLine();
+Console.WriteLine(int.TryParse(num, out a));
+
+n = 2;
+
+Console.WriteLine("Here we got: ");
+while (n < a) 
+{
+    Console.Write( n + ", ");
+    n = n + 2;
+}
