194165194938f7bc8638249540b326c7e87be0ca
Joru <jojooapgv@gmail.com>
2023-01-22 19:01:30 +0400

    moreless

---
+++ HW1/1More/less/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b;
+
+Console.WriteLine("Input integer number");
+string? num1 = Console.ReadLine();
+Console.WriteLine(int.TryParse(num1, out a));
+
+Console.WriteLine("...and one more");
+string? num2 = Console.ReadLine();
+Console.WriteLine(int.TryParse(num2, out b));
+
+if (a > b) Console.WriteLine(a + " > " + b);
+else Console.WriteLine(b + " > " + a);
+
+
