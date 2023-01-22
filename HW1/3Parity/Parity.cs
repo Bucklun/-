23e16bad891e9e029fb31b0a685c85714aaff943
Joru <jojooapgv@gmail.com>
2023-01-22 19:55:56 +0400

    parity

---
+++ HW1/3Parity/Parity.cs
@@ -0,0 +1,8 @@
+﻿int a;
+Console.WriteLine("Input number for check");
+string? num = Console.ReadLine();
+Console.WriteLine(int.TryParse(num, out a));
+
+if (a % 2 == 0) Console.WriteLine(a + " - even number");
+
+else Console.WriteLine(a + " - uneven number");
