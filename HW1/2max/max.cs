cb1b0c9b5f5191e395771562760328c2ced4f8e5
Joru <jojooapgv@gmail.com>
2023-01-22 19:27:02 +0400

    max

---
+++ HW1/2max/max.cs
@@ -0,0 +1,18 @@
+﻿int a, b, c;
+Console.WriteLine("You should input three integer numbers. First number ");
+string? num1 = Console.ReadLine();
+Console.WriteLine(int.TryParse(num1, out a));
+
+Console.WriteLine("Second number ");
+string? num2 = Console.ReadLine();
+Console.WriteLine(int.TryParse(num2, out b));
+
+Console.WriteLine("Third number ");
+string? num3 = Console.ReadLine();
+Console.WriteLine(int.TryParse(num3, out c));
+
+if (b > a) a = b;
+
+if (c > a) a = c;
+
+Console.WriteLine(a + " is the biggest number at this time ");
