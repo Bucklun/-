a4225d9358b71f9b1b485a82b72ee2670240ab99
Joru <jojooapgv@gmail.com>
2023-02-05 18:21:01 +0400

    firstExOfSecondLesson

---
+++ HW2/10SecondNum/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 0;
+while((a < 100) || (a >= 1000))
+{
+    Console.WriteLine("Enter a three-digit integer");
+    string? num = Console.ReadLine();
+    int.TryParse(num, out a); 
+}
+int b = (a / 10) % 10;
+Console.WriteLine($"Second digit in your number is {b}");
