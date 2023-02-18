040276a52a2de6e70b105b44381da60ac4452314
Joru <jojooapgv@gmail.com>
2023-02-18 16:06:53 +0400

    HW6

---
+++ HW6/41howMuch/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 46 - Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int EnteringNum (string str)
+{
+    int x;
+    Console.WriteLine(str);
+    try
+    {
+        x = Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+        Console.WriteLine("Try again");
+        x = EnteringNum(str);
+    }
+    return x;
+}
+int M = EnteringNum("How much numbers you want to enter?");
+
+int[] storage = new int[M];
+
+for (int i = 0; i < M; i++)
+{
+    storage[i] = EnteringNum($"{i+1} - Enter positive or negative integer number");
+}
+
+Console.WriteLine("Lets look, what we got: ");
+int pos = 0;
+foreach (var item in storage)
+{
+    Console.Write($"{item}|");
+    if (item > 0) pos++;
+}
+Console.WriteLine();
+Console.Write($"You had entered {pos} positive numbers");
+
---
+++ HW6/43linecrossing/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//            y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int EntNum(string wtf)
+{
+    int x = 0;
+    Console.WriteLine(wtf);
+    try
+    {
+        x = Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+        Console.Write("Nope");
+        x = EntNum(wtf);
+    }
+    return x;
+}
+
+Console.WriteLine("The equation of a line has the form y = kx + b, the programm find the point of intersection of two lines");
+
+int k1 = EntNum("Enter the k-coefficient of a first line");
+int b1 = EntNum("Enter the b-coefficient of a first line");
+int k2 = EntNum("Enter the k-coefficient of a second line");
+int b2 = EntNum("Enter the b-coefficient of a second line");
+
+double x = (b2 - b1) / (k1 - k2);
+x = Math.Round(x,2);
+
+double y = k1*x + b1;
+y = Math.Round(y,2);
+
+Console.WriteLine($"Lines, that you set, have a common point with coordinates ({x};{y})");
