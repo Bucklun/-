0c06e238745442908a6cbbdf7cc5cefc73c5d06f
Joru <jojooapgv@gmail.com>
2023-02-24 08:51:12 +0400

    64

---
+++ HW9/64fromNto1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int EnterNumber(string annotation)
+{
+    int x;
+    Console.WriteLine(annotation);
+    try
+    {
+        x = Convert.ToInt32(Console.ReadLine());
+    }
+    catch
+    {
+        Console.WriteLine("It's not an integer number, try again");
+        x = EnterNumber(annotation);
+    }
+    return x;
+}
+
+string NumbersRowFromGiven(int given)
+{
+    if (given == 0) return "";
+    else 
+    {
+        return $"{given} " + NumbersRowFromGiven(given - 1);
+    }
+}
+
+int N = EnterNumber("Enter the begining number");
+
+Console.WriteLine(NumbersRowFromGiven(N));
